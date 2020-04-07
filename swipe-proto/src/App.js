f15e20199cb942a12abc07a76b2e5fb49fab0249
Carl Jason Tapales <carldieztapales@gmail.com>
2020-04-07 14:48:44 +0800

    Updated Routing for Firebase Authentication

--- swipe-proto/src/App.js
+++ swipe-proto/src/App.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './containers/Login/index';
 import Home from './containers/Home/index';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/home" component={Home} />
-        </Switch>
-      </Router>
-    </React.Fragment>
-  );
+import firebase from './config/Firebase';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+    this.funcAuthListener();
+  }
+
+  funcAuthListener = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({user});
+      }
+      else {
+        this.setState({user:null});
+      }
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.user ?{Login} : {Home}}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
